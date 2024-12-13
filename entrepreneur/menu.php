2dcd828bbfa7592c8d0d5a36956d6c49bdb7daf0
aatfaoui aya <ayaaatfaoui38@gmail.com>
2024-12-13 17:13:52 +0100

    commit

--- entrepreneur/menu.php
+++ entrepreneur/menu.php
@@ -83,6 +83,10 @@
       justify-content: center;
       align-items: center;
     }
+    .menu a.active {
+      background-color: #18B7BE !important; /* Bleu pour l'élément actif */
+      color: white !important; /* Texte en blanc */
+    }
 
     @media (max-width: 768px) {
       .sidebar {
@@ -139,5 +143,25 @@
       document.getElementById('toggle-btn').style.display = 'flex';
     }
   </script>
+  <script>
+     // Récupérer tous les liens du menu
+     const menuLinks = document.querySelectorAll('.menu a');
+
+// Fonction pour vérifier l'URL actuelle
+function setActiveLink() {
+  const currentPath = window.location.pathname;
+  menuLinks.forEach(link => {
+    const linkPath = new URL(link.href).pathname;
+    if (currentPath === linkPath) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
+// Exécuter la fonction lors du chargement de la page
+window.addEventListener('load', setActiveLink);
+</script>
 </body>
 </html>
